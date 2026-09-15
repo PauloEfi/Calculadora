ee496cd1644d3043e82e81e48c4aa4b88b3588c9
PauloEfi <paulofelipe977@gmail.com>
2026-09-14 22:25:33 -0300

    1

--- calculadora.py
+++ calculadora.py
@@ -13,7 +13,7 @@ def multiplicar(a, b):
 
 def dividir(a, b):
     if b == 0:
-        raise ZeroDivisionError("Não é possível divisão por zero")
+        raise ZeroDivisionError("Não é possível dividir por zero")
     return a / b
 
 def adicionar(valor):
